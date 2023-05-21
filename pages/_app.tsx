aec2423fc7fce669107713f3b14daef5c46041fb
Moinak Majumdar <moinak2030@gmail.com>
2023-05-21 21:47:04 +0530

    vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
 import NextNProgress from 'nextjs-progressbar';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
+import 'react-toastify/dist/ReactToastify.css';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import PageLoader from '@/components/tools/PageLoader';
 import { FunctionProvider } from '@/context/FunctionContext';
 import { AuthProvider } from '@/context/AuthContext';
-import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
+      <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -30,8 +30,8 @@ export default function App({ Component, pageProps }: AppProps) {
           </FunctionProvider>
         </AuthProvider>
       </PageLoader>
-      <ToastContainer newestOnTop={true} autoClose={3000} pauseOnHover />
       <Analytics />
+      <ToastContainer newestOnTop={true} autoClose={3000} pauseOnHover />
     </>
   )
 }
